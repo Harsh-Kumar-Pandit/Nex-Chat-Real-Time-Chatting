f3559c377f6c0ff2e0294f1bc4ed2c7fdef05721
Harsh <harshkumarpandit2004@gmail.com>
2026-03-08 12:41:19 +0530

    Update AuthRoutes.js

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -4,15 +4,15 @@ import { verifyToken } from "../middlewares/AuthMiddleware.js";
 import multer from "multer";
 
 const authRoutes = Router();
-//multer add files to req.files
-const upload = multer({ dest: "uploads/profiles/"});
+
+const upload = multer({ dest: "uploads/profiles/" });
 
 authRoutes.post("/signup", signup);
 authRoutes.post("/login", login);
-authRoutes.get("/user-info",verifyToken, getUserInfo)
-authRoutes.post("/update-profile",verifyToken, updateProfile)
-authRoutes.post("/add-profile-image",verifyToken,  upload.single("profile-image"),addProfileImage)
-authRoutes.delete("/remove-pofile-image", verifyToken, removeProfileImage)
-authRoutes.post("/logout", verifyToken, logOut)
+authRoutes.get("/user-info", verifyToken, getUserInfo);
+authRoutes.post("/update-profile", verifyToken, updateProfile);
+authRoutes.post("/add-profile-image", verifyToken, upload.single("profile-image"), addProfileImage);
+authRoutes.delete("/remove-profile-image", verifyToken, removeProfileImage); 
+authRoutes.post("/logout", verifyToken, logOut);
 
 export default authRoutes;
